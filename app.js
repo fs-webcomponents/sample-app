e996543ced6c168567d91a4d738d1d9e9b478d07
Justin <justincyork@gmail.com>
2015-09-28 21:53:48 +0000

    data binding from routes

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-page('/person/:personId', displayPerson);
-page('/person', currentUser, displayPerson);
-
-// Forward to /person by default to load the current user person's page
-page('*', function(ctx){
-  page('/person');
-});
-
-// Use hashbangs to get a one page app.
-// For some reason this has to be after the route declarations.
-page({
-  hashbang: true
-});
-
-function currentUser(context, next){
-  setTimeout(function(){
-    context.personId = 'foo';
-    next();
+window.addEventListener('WebComponentsReady', function() {
+  
+  page('/person/:personId', displayPerson);
+  page('/person', currentUser, displayPerson);
+  
+  // Forward to /person by default to load the current user person's page
+  page('/', function(ctx){
+    page('/person');
   });
-}
+  
+  // Use hashbangs to get a one page app.
+  // For some reason this has to be after the route declarations.
+  page({
+    hashbang: true
+  });
+  
+  function currentUser(context, next){
+    setTimeout(function(){
+      context.params.personId = 'foo';
+      next();
+    });
+  }
+  
+  function displayPerson(context){
+    console.log(context);
+    document.getElementById('app').personId = context.params.personId;
+  }
 
-function displayPerson(context){
-  console.log(context);
-}
+});
