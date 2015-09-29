d4250719052be90838db88302e0fecf940ae30cf
Justin <justincyork@gmail.com>
2015-09-29 15:38:36 +0000

    load user and person after authentication

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 window.addEventListener('WebComponentsReady', function() {
   
   var app = document.getElementById('app'),
-      client = document.getElementById('client').client;
+      $client = document.getElementById('client'),
+      client = $client.client;
   
-  page('/person/:personId', currentUser, displayPerson);
-  page('/person', currentUser, displayPerson);
+  page('/person/:personId', ensureAuth, currentUser, displayPerson);
+  page('/person', ensureAuth, currentUser, displayPerson);
   
   // Forward to /person by default to load the current user person's page
   page('/', function(ctx){
@@ -17,6 +18,25 @@ window.addEventListener('WebComponentsReady', function() {
     hashbang: true
   });
   
+  /**
+   * Make sure the user is authenticated.
+   */
+  function ensureAuth(context, next){
+    if(!client.hasAccessToken()){
+      $client.addEventListener('authenticated-changed', function(event){
+        if(event.detail.value){
+          next();
+        }
+      });
+    } else {
+      next();
+    }
+  }
+  
+  /**
+   * Get the current user so we can show the username in the title.
+   * We might also need the current user's person id.
+   */
   function currentUser(context, next){
     client.getCurrentUser().then(function(response){
       context.user = response.getUser();
@@ -24,7 +44,13 @@ window.addEventListener('WebComponentsReady', function() {
     });
   }
   
+  /**
+   * Show the requested person of the current user's person
+   */
   function displayPerson(context){
+    
+    // Use the current user's person id if there was no person id
+    // specified in the url
     var personId = context.params.personId;
     if(!personId){
       personId = context.user.getPersonId();
